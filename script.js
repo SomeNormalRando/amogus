5934fc1a47887c1128f419a817fe2881741f964c
Rando <67262768+SomeNormalRando@users.noreply.github.com>
2021-09-04 14:35:39 +0800

    ឵឵

---
+++ script.js
@@ -0,0 +1,1 @@
+const b=document.getElementById("b");document.getElementById("d").innerHTML="I'm not a robot",b.addEventListener("click",()=>{const e=document.getElementById("c"),t=t=>{setTimeout(()=>{e.style.opacity=t},750*t)};for(let e=0;e<=100;e+=4)t(e/100),100===e&&setTimeout(()=>{window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab","_self")},1e3)});
